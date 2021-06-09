f9221bf2f77fd414251d8bf97c7291ad40421342
Clark Evangelista <Clark Evangelista@DESKTOP-CFORFPG>
2021-06-09 19:30:05 +0900

    content delete finish

---
+++ src/components/BooksList.jsx
@@ -0,0 +1,106 @@
+import { Avatar, Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, Divider, IconButton, List, ListItem, ListItemAvatar, ListItemSecondaryAction, ListItemText, Paper, Typography, useMediaQuery, useTheme } from "@material-ui/core";
+import { Book, Delete } from "@material-ui/icons";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function BooksList() {
+    let [loading, setLoading] = useState(true);
+    let [book, setBooks] = useState([]);
+    let [openDialog, setOpenDialog] = useState(false);
+    const url = "http://localhost:8111/books"
+    let getBooks = async () => {
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data);
+        setBooks(data)
+        setLoading(false)
+    }
+    let [title,setTitle] = useState("");
+    let [id,setId] = useState("");
+    useEffect(() => {
+        getBooks();
+    }, [])
+    return (
+        <Paper elevation={3}>
+            {loading ? (<CircularProgress />) :
+         
+                <List>
+                    <Typography variant="h3" gutterBottom style={{padding:"10px 30px"}}>
+                       Book List
+                    </Typography>
+                    <Divider />
+                    {book.map((e) =>
+                        <ListItem key={e._id}>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <Book />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary={e.title} secondary={e.authors.join(", ")} />
+                            <ListItemSecondaryAction>
+                                <IconButton edge="end" aria-label="delete" onClick={()=>{
+                                    setTitle(e.title);
+                                    setId(e._id)
+                                    setOpenDialog(true);
+                                }} color="secondary">
+                                    <Delete  />
+                                </IconButton>
+                            </ListItemSecondaryAction>
+                        </ListItem>
+                    )}
+                </List>
+            }
+            {openDialog&&
+                <DeleteDialog delete_id={id} title={title} dialogOpen={setOpenDialog}/>
+            }
+        </Paper>
+    )
+}
+
+function DeleteDialog({title, delete_id, dialogOpen}){
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+    const url = "http://localhost:8111/books"
+    let deleteItem = async(id) =>{
+        console.log(id);
+        let newAxios = {
+            'method':"delete",
+            'url': url,
+            'data':{
+                '_id':id
+            }
+        }
+        try{
+            let response =  await axios(newAxios);
+            console.log(response.status)
+        }
+        catch(e){
+            console.log(e)
+        }
+    }
+  
+    const handleClose = () => {
+      dialogOpen(false);
+    };
+    return(
+        <Dialog
+        fullScreen={fullScreen}
+        open={true}
+        onClose={handleClose}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogTitle id="responsive-dialog-title">{"Are you sure you want to delete "+title+ "?"}</DialogTitle>
+        <DialogContent>
+         
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus onclick={()=>deleteItem(delete_id)}>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,99 @@
+import { AppBar, Button, Container, Divider, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText, makeStyles, Toolbar, Typography } from "@material-ui/core";
+import { Book, Category, ChevronLeft, ListAlt, Menu, Person } from "@material-ui/icons";
+import React, { useState } from "react";
+import { Route, Switch } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
+import BooksList from "./BooksList";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+
+
+export default function Main() {
+    const classes = useStyles();
+    let [open, setOpen] = useState(false);
+    return (
+        <div>
+            <Router>
+                <React.Fragment>
+                    <Drawer
+                        className={classes.drawer}
+                        variant="persistent"
+                        anchor="left"
+                        open={open}
+                        classes={{
+                            paper: classes.drawerPaper,
+                        }}
+                    >   
+                    <Typography variant="h5" style={{marginTop:"15px"}}>
+                        <div className={classes.drawerHeader}>
+                            <IconButton onClick={() => setOpen(false)}>
+                                <ChevronLeft />
+                            </IconButton>
+                            Menu
+                        </div> 
+                        </Typography>
+                        <Divider />
+                        <List>
+                            {['Books', 'Categories', 'Members', 'Issue Book'].map((text, index) => (
+                                <a key={text} href={"/" + text.split(" ")[0].toLowerCase()} >
+                                    <ListItem button onClick={() => {
+                                        console.log(text.split(" ")[0])
+                                        let path = "/" + text.split(" ")[0].toLowerCase();
+                                        console.log(path)
+                                    }}>
+                                        <ListItemIcon>{index === 0 ? <Book /> : index === 1 ? <Category /> : index === 2 ? <Person /> : <ListAlt />}</ListItemIcon>
+                                        <ListItemText primary={text} />
+                                    </ListItem>
+                                </a>
+                            ))}
+                        </List>
+                        <Divider />
+                    </Drawer>
+                </React.Fragment>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={() => setOpen(true)}>
+                            <Menu />
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title}>
+                            My Library Management
+                </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+
+                <Switch>
+                    <Container fixed>
+                        <Route exact path="/" >
+                            <h1> Welcome to MyLibrary!</h1>
+                        </Route>
+                        <Route path="/books">
+                            <BooksList />
+                        </Route>
+                        <Route path="/categories">
+                            <h1>Categories</h1>
+                        </Route>
+                        <Route path="/members">
+                            <h1>Members </h1>
+                        </Route>
+                        <Route path="/issue">
+                            <h1>Issue Books </h1>
+                        </Route>
+                    </Container>
+                </Switch>
+
+            </Router>
+        </div>
+    )
+}
